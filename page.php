f46cdfd9cd958b3827ac0e84d4442d0021d27d58
jackmachin <jack.machin125@gmail.com>
2015-02-24 13:25:23 +0000

    Semantic

--- page.php
+++ page.php
@@ -8,10 +8,14 @@
     
           <?php if (have_posts()) : while (have_posts()) : the_post(); ?>
           
+              <article id="post-<?php the_ID(); ?>" <?php post_class(); ?> role="article" itemscope itemtype="http://schema.org/BlogPosting">
+          
               <?php
                 // the content (pretty self explanatory huh)
                 the_content();
               ?>
+              
+              </article>
 
           <?php endwhile; else : ?>
 
